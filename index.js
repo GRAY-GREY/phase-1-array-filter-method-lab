18ef2fb9b32c8960a575f5ece9d9d32bd5330396
+ <+>
2021-10-29 17:15:23 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(target =>
+        (target.toUpperCase()) == (name.toUpperCase())
+    );
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(target => 
+        target.indexOf(letters) === 0
+    );
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(match => 
+        match['name'] === string    
+    );
+}
